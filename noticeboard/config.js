e90e0dbcca9979cb943045004aa4eb2a33765006
basementuniverse <gordonlarrigan@googlemail.com>
2026-09-23 23:37:27 +0100

    Add live noticeboard sample token

--- noticeboard/config.js
+++ noticeboard/config.js
@@ -1,2 +1,2 @@
 // Paste the value of your Notice Board token here (see step 5 in README.md)
-const JSONPAD_TOKEN = 'PASTE YOUR NOTICE BOARD TOKEN HERE';
+const JSONPAD_TOKEN = 'f6djguU6IRpkBGxfBtRecGznY6n6IGcb';
